1b5df5792dd4ff2a152d808599465b84c4f9b302
Alisher2011 <djalolovataxmina@gmail.com>
2023-04-07 12:02:17 +0500

    index.py uzgartirdim

---
+++ index.py
@@ -0,0 +1,93 @@
+# n=int(input("son kiriting : "))
+# for i in range(n):
+#     print("salom olam")
+
+
+# n=int(input("son kiriting : "))
+# for i in range(n):
+#     print(i)
+
+
+# n=int(input("son kiriting : "))
+# for i in range(n):
+#     if n%2==0:
+#         print(0)
+#     else:
+#         print(1)
+
+
+# n=int(input("sonnni kiriting : "))
+# for i in range(n):
+#     if i%2==0: 
+#         print(i)
+
+
+# n=int(input("sonnni kiriting : "))
+# for i in range(n):
+#     if i%2==1: 
+#         print(i)
+
+
+# n=int(input("sonnni kiriting : "))
+# sum1=0
+# for i in range(n):
+#     sum1=sum1+i
+# print(sum1)
+
+
+# n=int(input("sonnni kiriting : "))
+# Sum=0
+# for i in range(n):
+#     if i%2==1: 
+#         Sum=Sum+i
+#         print(Sum)
+# print(Sum)
+
+
+# n=int(input("sonnni kiriting : "))
+# Sum=0
+# for i in range(n):
+#     if i%2==0: 
+#         Sum=Sum+i
+#         print(Sum)
+# print(Sum)
+
+
+# n=int(input("sonnni kiriting : "))
+# s = 0
+# for i in range(1,n+1):
+#     if i%10==0:
+#         s += i
+#         print(s)
+
+
+# n=int(input("sonnni kiriting : "))
+# for i in range(n):
+#     if i>0: 
+#         print(i)
+
+
+# n=int(input("sonnni kiriting : "))
+# for i in range(n):
+#     if i<0: 
+#         print(i)
+
+
+# n=int(input("sonnni kiriting : "))
+# for i in range(n):
+#     if i%2==0: 
+#         print(i)
+
+
+# n=int(input("sonnni kiriting : "))
+# for i in range(n):
+#     if i%2==1: 
+#         print(i)
+
+
+# n=int(input("sonnni kiriting : "))
+# for i in range(n):
+#     if n<0: 
+#         print(abs(n))
+#     else:
+#         print(i)
